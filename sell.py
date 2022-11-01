75dc193f04b48818c38651e9261b88629bf3d035
Young <cksdudh@naver.com>
2022-11-01 21:16:10 +0900

    real final

--- sell.py
+++ sell.py
@@ -7,8 +7,8 @@ import datetime
 # Secret ke
 # xhcrm3uvqNcxDNBbTyb7T4joUUiEFKj7K6nlYWD6
 
-access = "WX84pZEWXv11BRmWRqj3hfl6xyxQWYty0uSJ0R05"
-secret = "xhcrm3uvqNcxDNBbTyb7T4joUUiEFKj7K6nlYWD6"
+access = "WX84pZEWXv11BRmWRqj3hfl6xyxQWYty0uSJYR05"
+secret = "xhcrm3uvqNcxDNBbTyb7T4joUUiEFKj7K6nl0WD6"
 
 # ticker는 목표 종목, k는 희망하는 k값
 def get_target_price(ticker, k):
@@ -46,6 +46,29 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
 # 자동매매 시작
+while True:
+    try:
+        # 시간 설정
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-BTC")
+        end_time = start_time + datetime.timedelta(days=1) # 끝나는 시간은 다음날 0900시
+        #시간에 따라서 if문
+        if start_time + datetime.timedelta(seconds=10) < now < end_time: # 09시로 하면 무한루프가 되니까
+            target_price = get_target_price("KRW-BTC", 0.5)
+            current_price = get_current_price("KRW-BTC")
+            if target_price < current_price:
+                btc = get_balance("BTC")
+                if btc > 0.00001:
+                    upbit.sell_market_order("KRW-BTC", btc*0.9995)
+        else: # 당일 시가에 전량 매수
+            krw = get_balance("KRW")
+            if krw > 5000:
+                upbit.buy_market_order("KRW-BTC", krw*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
+
 # while True:
 #     try:
 #         # 시간 설정
@@ -57,9 +80,9 @@ print("autotrade start")
 #             target_price = get_target_price("KRW-BTC", 0.5)
 #             current_price = get_current_price("KRW-BTC")
 #             if target_price < current_price:
-#                 krw = get_balance("KRW")
-#                 if krw > 5000:
-#                     upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                # krw = get_balance("KRW")
+                # if krw > 5000:
+                #     upbit.buy_market_order("KRW-BTC", krw*0.9995)
 #         else: # 당일 종가에 전량 매도
 #             btc = get_balance("BTC")
 #             if btc > 0.00008:
